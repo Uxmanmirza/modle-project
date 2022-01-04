f61393845dc1e96c091a045901cd2c311a3f469a
Usman Ali <91782435+Uxmanmirza@users.noreply.github.com>
2022-01-05 00:40:43 +0500

    complete project

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const overLay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+//Modal opening
+const openModal = function () {
+  console.log('working');
+  modal.classList.remove('hidden');
+  overLay.classList.remove('hidden');
+};
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+//Modal closing
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overLay.classList.add('hidden');
+};
+btnCloseModal.addEventListener('click', closeModal);
+overLay.addEventListener('click', closeModal);
+
+//Modal closing by Esc
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
